591ea01ec5b23061696ad0f7513408d9e9b1569f
Ronyszu <ronyszu@gmail.com>
2020-12-28 11:28:26 -0300

    updated to .NET core 5.0.1

--- TSAnalyzer/Startup.cs
+++ TSAnalyzer/Startup.cs
@@ -30,7 +30,7 @@ namespace TSAnalyzer
         {
             services.AddCors();
             DependencyContainer.RegisterServices(services);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options=>options.EnableEndpointRouting=false).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
